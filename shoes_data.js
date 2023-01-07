de04d44e534e2321cc0a06680da2a615effd0d59
ShpaquelleOneal <109593992+ShpaquelleOneal@users.noreply.github.com>
2023-01-07 19:10:22 +0200

    pictures added

---
+++ shoes_data.js
@@ -0,0 +1,32 @@
+var shoes = [
+    {
+        "Name": "Nike Offset",
+        "Color": "Brown / Black",
+        "Gender": "Male",
+        "Size": "Adult"
+    },
+    {
+        "Name": "Nike Everyday",
+        "Color": "Black / White",
+        "Gender": "Male",
+        "Size": "Adult"
+    },
+    {
+        "Name": "Nike Flat",
+        "Color": "Black / White",
+        "Gender": "Male",
+        "Size": "Adult"
+    },
+    {
+        "Name": "Nike Pure",
+        "Color": "White",
+        "Gender": "Male",
+        "Size": "Adult"
+    },
+    {
+        "Name": "Nike Running",
+        "Color": "Black / White",
+        "Gender": "Male",
+        "Size": "Adult"
+    }
+]
