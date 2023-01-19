552b1aba91544bd08981a245d56bce629de57017
ShpaquelleOneal <109593992+ShpaquelleOneal@users.noreply.github.com>
2023-01-19 15:02:59 +0200

    design finished

--- shoes_data.js
+++ shoes_data.js
@@ -1,32 +1,30 @@
 var shoes = [
     {
-        "Name": "Nike Offset",
-        "Color": "Brown / Black",
+        "Name": "Archegos-1",
+        "Color": "Black",
         "Gender": "Male",
-        "Size": "Adult"
+        "Release date": "2022-12-25",
+        "Description": "Step outside your comfort zone and into bold style with the Nike Air Max Plus, a Tuned Air experience that offers premium stability and unbelievable cushioning as you explore new horizons. Featuring airy mesh, bold gradient colours and the wavy design lines of the original, they celebrate defiant style."
     },
     {
-        "Name": "Nike Everyday",
+        "Name": "CityLook-22",
         "Color": "Black / White",
         "Gender": "Male",
-        "Size": "Adult"
+        "Release date": "2023-01-25",
+        "Description": "Meet the street-level favourite you'll want to wear on repeat—just like the Swoosh designs running down the pull tab on the heels. With its stretchy bootie-like upper, crisp leather accents, Nike Air cushioning and futuristic heel cage, the Huarache is bragging rights for your feet."
     },
     {
-        "Name": "Nike Flat",
-        "Color": "Black / White",
-        "Gender": "Male",
-        "Size": "Adult"
+        "Name": "Rocky_Citadel",
+        "Color": "Beige",
+        "Gender": "Female",
+        "Release date": "2022-12-27",
+        "Description": "We design the Elite boot for you and the world's biggest stars to give you high-level quality because you demand greatness from yourself and your footwear."
     },
     {
-        "Name": "Nike Pure",
-        "Color": "White",
-        "Gender": "Male",
-        "Size": "Adult"
+        "Name": "Panther-white-02",
+        "Color": "Pink",
+        "Gender": "Female",
+        "Release date": "2022-11-01",
+        "Description": "Paying homage to both heritage and innovation, we've blended two icons to go beyond what's expected."
     },
-    {
-        "Name": "Nike Running",
-        "Color": "Black / White",
-        "Gender": "Male",
-        "Size": "Adult"
-    }
 ]
